1c91419f7b9101c9fd15c7dbf9674e695303d656
Alexander Januar <alexanderjanuar16@gmail.com>
2025-10-15 10:51:09 +0800

    AdminUpdate

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@ Route::get('/', App\Livewire\Home\Index::class)->name('home.index');
 Route::get('/karir', Index::class)->name('career.index');
 Route::get('/karir/{jobPosting}/apply', \App\Livewire\Career\JobApplication::class)->name('career.apply');
 
-Route::redirect('/laravel/login', '/login')->name('login');
+// Route::redirect('/laravel/login', '/login')->name('login');
 
