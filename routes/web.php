f134e1690e7cdd1256fca7d03b05944beee4d711
Alexander Januar <alexanderjanuar16@gmail.com>
2025-10-15 10:47:55 +0800

    Login

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,3 @@ Route::get('/karir', Index::class)->name('career.index');
 Route::get('/karir/{jobPosting}/apply', \App\Livewire\Career\JobApplication::class)->name('career.apply');
 
 // Route::redirect('/laravel/login', '/login')->name('login');
-
-Route::get('/login', function () {
-    $prevUrl = url()->previous();
-
-    if (! $prevUrl) {
-        abort(404); // or redirect some where
-    }
-
-    $path = parse_url($prevUrl, PHP_URL_PATH);
-
-    $panelId = explode('/', trim($path, '/'))[0];
-
-    if (! in_array($panelId, array_keys(Filament::getPanels()))) {
-        abort(404);
-    }
-
-    return redirect(route("filament.{$panelId}.auth.login"));
-})->name('login');
